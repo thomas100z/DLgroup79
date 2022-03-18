9aba01d87c51522adfd0fd8d53d4e8291dddcd46
thomas100z <thomas.zuiker@hotmail.com>
2022-03-18 14:26:41 +0100

    continue on model

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torch
 EPOCH = 1
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 OUT_CHANNEL = 1
+LOAD_PATH = None #'./models/18-14:08OrganNet.pth'
 
 # get the data from the dataloader, paper: batch size = 2
 training_data, test_data = get_data()
@@ -27,6 +28,10 @@ net.to(DEVICE)
 
 # paper: adam 0.001 initial, reduced by factor 10 every 50 epoch
 optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+# optional restore checkpoint
+if LOAD_PATH:
+    net.load_checkpoint(LOAD_PATH,optimizer, 0.001)
 
 # focal loss + dice loss
 criterion = torch.nn.CrossEntropyLoss()
@@ -54,7 +59,7 @@ for epoch in range(EPOCH):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        print(f"[EPOCH {epoch + 1}] sample: ({i}/{len(train_dataloader)})\tloss: {loss.item()}")
+        print(f"[EPOCH {epoch + 1}] sample: ({i}/{len(train_dataloader)})\tcombined loss: {loss.item()}\tloss_focal: {loss_focal.item()}\tloss_dice: {loss_dice.item()}")
 
     with torch.no_grad():
         for j, data in enumerate(validation_dataloader):
